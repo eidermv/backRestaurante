64e7127f8929bb43c1a7fc92e4638180c7ca8d02
Eider Arley Muñoz <eider205v@gmail.com>
2020-04-05 18:33:29 -0500

    EAMV cnfiguracion inicial

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
+var dotenv = require('dotenv');
+var cors = require('cors');
+
+//carga las variables de entorno
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// soporte cors y headers de seguridad
+app.use(cors());
+app.use(helmet());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
